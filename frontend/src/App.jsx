b34076334af5e8c8200b0350a1222876185a0086
Jamesgab29 <jamesgabcotiangco29@gmail.com>
2025-11-20 17:42:11 +0800

    Profile page added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,8 +2,10 @@ import React from "react";
 import { Routes, Route } from "react-router-dom";
 import LandingPage from "./pages/Landing-page.jsx";
 import LoginPage from "./pages/Login-page.jsx";
-import RegisterPage from "./pages/Register-page.jsx"; // Import your register page
+import RegisterPage from "./pages/Register-page.jsx";
 import ShopPage from "./pages/Shop.jsx";
+import DashboardPage from "./pages/Dashboard-page.jsx";
+import ProfilePage from "./pages/Profile-page.jsx";
 
 function App() {
   return (
@@ -12,6 +14,8 @@ function App() {
       <Route path="/login" element={<LoginPage />} />
       <Route path="/register" element={<RegisterPage />} />
       <Route path="/shop" element={<ShopPage />} />
+      <Route path="/dashboard" element={<DashboardPage />} />
+       <Route path="/profile" element={<ProfilePage />} />
     </Routes>
   );
 }
